f86413a0559cd56aa10c8926e96d0aae830e303e
Luisa <luisa.santos042@gmail.com>
2023-10-15 17:31:10 -0300

    add route to get tasks
    get array of tasks for the logged user

--- src/main/java/br/com/nunius/todolist/task/TaskRepositoryInterface.java
+++ src/main/java/br/com/nunius/todolist/task/TaskRepositoryInterface.java
@@ -1,9 +1,10 @@
 package br.com.nunius.todolist.task;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepositoryInterface extends JpaRepository<TaskModel, UUID> {
-    
+    List<TaskModel> findByUserId(UUID userId);
 }
